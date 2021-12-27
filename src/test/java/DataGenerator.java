6fb8fbd90737eacfe2ab7647922a90ba4bfe172f
shanedasha <chirkova_99@inbox.ru>
2021-12-27 14:38:38 +0300

    Fix test

---
+++ src/test/java/DataGenerator.java
@@ -0,0 +1,52 @@
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String generateCity(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.address().city();
+    }
+
+    public static String generateName(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.name().fullName();
+    }
+
+    public static String generatePhone(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        return faker.phoneNumber().phoneNumber();
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            Faker faker = new Faker(new Locale(locale));
+            return new UserInfo(
+                    generateCity(locale),
+                    generateName(locale),
+                    generatePhone(locale)
+            );
+        }
+    }
+
+    @Value
+    public static class UserInfo {
+        String city;
+        String name;
+        String phone;
+    }
+}
+
--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -1,64 +1,33 @@
 import com.codeborne.selenide.Configuration;
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
-import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DeliveryCardTest {
-    String planningDate = generateDate(4);
-    String newPlaningDate = generateNewDate(6);
-    private static Faker faker;
+    String planningDate = DataGenerator.generateDate(4);
+    String newPlaningDate = DataGenerator.generateDate(6);
 
-    String firstName = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String phoneNumber = faker.phoneNumber().phoneNumber();
-    String city = faker.address().city();
-
-
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    public String generateNewDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    }
-
-    @BeforeAll
-    public  static  void setUpAll() {
-        faker = new Faker(new Locale("ru"));
-    }
 
     @Test
     public void shouldChangeDateAndSet() {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
-        $("[data-test-id='city'] input").sendKeys(city);
+        $("[data-test-id='city'] input").sendKeys(DataGenerator.generateCity("ru"));
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(planningDate);
-        $("[data-test-id='name'] input").sendKeys(lastName);
-        $("[data-test-id='name'] input").sendKeys(Keys.ESCAPE);
-        $("[data-test-id='name'] input").sendKeys(firstName);
-        $("[data-test-id='phone'] input").sendKeys(phoneNumber);
+        $("[data-test-id='name'] input").sendKeys(DataGenerator.generateName("ru"));
+        $("[data-test-id='phone'] input").sendKeys(DataGenerator.generatePhone("ru"));
         $("[data-test-id='agreement']").click();
         $(".button").click();
-        $("[data-test-id='success-notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + planningDate));
+        $("[data-test-id='success-notification']").shouldBe(visible).shouldHave(text("Встреча успешно запланирована на " + planningDate));
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='date'] input").setValue(newPlaningDate);
         $(".button").click();
-        $("[data-test-id='replan-notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldBe(visible).shouldHave(exactText("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+        $("[data-test-id='replan-notification'] .notification__content").shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
         $("div.notification .button").click();
-        $("[data-test-id='success-notification']").shouldBe(visible, Duration.ofSeconds(15));
-        $(".notification__content").shouldBe(visible).shouldHave(exactText("Встреча успешно забронирована на " + newPlaningDate));
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldHave(text("Встреча успешно запланирована на " + newPlaningDate));
 
     }
 
